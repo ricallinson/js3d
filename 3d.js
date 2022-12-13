e0628ab878eb4d5911419bb0ff51473b90e8054c
Ric Allinson <rallinson@medicalert.org>
2022-12-13 14:29:43 -0800

    Working the code

--- 3d.js
+++ 3d.js
@@ -3,40 +3,39 @@
 // Geometry of a Model
 var cubes = [];
 class Cube {
-  constructor(a) {
-    this.offset = a;
-    this.color = 'rgba(' + Math.floor(Math.random() * 256) + ',   ' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', 1)';
-    var v = [-1, -1, -1,     1, -1, -1,    1, -1, 1,    
-             -1, -1, 1,     -1, 1, -1,     1, 1, -1,
-             1, 1, 1,       -1, 1, 1];
-    var t = [0, 1, 2,    2, 3, 0,    4, 5, 6,    6, 7, 4,
-             0, 1, 4,    1, 4, 5,    1, 2, 5,    2, 5, 6,
-             2, 3, 6,    3, 6, 7,    3, 0, 7,    0, 7, 4
-    ];
-    this.dx = -25 + 5 * (this.offset / 10);
-    this.dy = -3 + Math.random() * 6;
-    this.dz = -25 + 5 * (this.offset % 10);
-    this.trngls = [];
-    this.points = [];
-    for (var j = 0; j < v.length; j += 3) {
-      this.points.push(this.dx + v[j], this.dy + v[j + 1], this.dz + v[j + 2]);
+    constructor(a) {
+        this.offset = a;
+        var v = [-1, -1, -1, 1, -1, -1, 1, -1, 1,    
+            -1, -1, 1, -1, 1, -1, 1, 1, -1,
+            1, 1, 1, -1, 1, 1];
+        var t = [0, 1, 2, 2, 3, 0, 4, 5, 6, 6, 7, 4,
+            0, 1, 4, 1, 4, 5, 1, 2, 5, 2, 5, 6,
+            2, 3, 6, 3, 6, 7, 3, 0, 7, 0, 7, 4
+        ];
+        this.dx = -25 + 5 * (this.offset / 10);
+        this.dy = -3 + Math.random() * 6;
+        this.dz = -25 + 5 * (this.offset % 10);
+        // Plot
+        var j;
+        this.points = [];
+        for (j = 0; j < v.length; j += 3) {
+            this.points.push(this.dx + v[j], this.dy + v[j + 1], this.dz + v[j + 2]);
+        }
+        this.triangles = [];
+        for (j = 0; j < t.length; j++) {
+            this.triangles.push(this.offset * 8 + t[j]);
+        }
     }
-    for (var j = 0; j < t.length; j++) {
-      this.trngls.push(this.offset * 8 + t[j]);
-    }
-  }
 }
 
 for (var i = 0; i < 100; i++) {
-  cubes.push(new Cube(i));
+    cubes.push(new Cube(i));
 }
 
 var canvas = document.getElementById("viewport");
 var ctx = canvas.getContext("2d");
 var height = canvas.clientWidth;
 var width = canvas.clientHeight;
-
-console.log(height, width);
 
 var posX = 0;
 var posY = 0;
@@ -53,131 +52,134 @@ var rotateLeft=false;
 var rotateRight=false;
 
 document.body.addEventListener('keydown', function(e) {
-  updateKeys(e.keyCode, true);
+    updateKeys(e.keyCode, true);
 });
 
 document.body.addEventListener('keyup', function(e) {
-  updateKeys(e.keyCode, false);
+    updateKeys(e.keyCode, false);
 });
 
 function updateKeys(code,val) {
-  switch (code) {
+    switch (code) {
     case 37:
-      moveLeft=val;
-      break; //moveLeft key
+        moveLeft=val;
+        break; //moveLeft key
     case 87:
-      moveUp=val;
-      break; //Up key
+        moveUp=val;
+        break; //Up key
     case 39:
-      moveRight=val;
-      break; //moveRight key
+        moveRight=val;
+        break; //moveRight key
     case 83:
-      moveDown=val;
-      break; //Down key
+        moveDown=val;
+        break; //Down key
     case 38:
-      moveForwards=val;
-      break; //w key
+        moveForwards=val;
+        break; //w key
     case 40:
-      moveBackwards=val;
-      break; //s key
+        moveBackwards=val;
+        break; //s key
     case 65:
-      rotateLeft=val;
-      break; //a key
+        rotateLeft=val;
+        break; //a key
     case 68:
-      rotateRight=val;
-      break; //d key
+        rotateRight=val;
+        break; //d key
     // default:
     //   console.log(code);
-  }
+    }
 }
 
-var positions = [];
+function getPositions(cubes) {
+    var points = [];
+    var triangles = [];
+    for (var i = 0; i < cubes.length; i++) {
+        points.push(...cubes[i].points);
+        triangles.push(...cubes[i].triangles);
+    }
+    return [points, triangles];
+}
 
 function update() {
-  if(moveLeft) posX+=2;
-  if(moveRight) posX-=2;
-  if(moveUp) posZ-=2;
-  if(moveDown) posZ+=2;
-  if(moveForwards) posY+=2;
-  if(moveBackwards) posY-=2;
-  if(rotateLeft) posR+=2;
-  if(rotateRight) posR-=2;
-  positions = [];
-  var ang = posR * 0.0123;
-  var s = Math.sin(ang);
-  var c = Math.cos(ang);
-  var allPoints = [];
-  var allTrngls = [];
-  var allColors = [];
-  // What does this do?
-  var matrix = [c, 0, -s, (posX * 0.123), 0, 1, 0, (posY * 0.123), s, 0, c, (posZ * 0.123)];
-  // console.log('X', posX, 'Y', posY, 'Z', posZ, 'R', posR);
-  ctx.clearRect(0, 0, height, width);
-  for (var i = 0; i < cubes.length; i++) {
-    allPoints.push(...cubes[i].points);
-    allTrngls.push(...cubes[i].trngls);
-    allColors.push(cubes[i].color);
-  }
-  draw(allPoints, allTrngls, matrix, allColors);
-  positions.sort(sortZ);
-  paintFill(positions);
-  window.requestAnimationFrame(update);
+    if(moveLeft) posX += 2;
+    if(moveRight) posX -= 2;
+    if(moveUp) posZ -= 2;
+    if(moveDown) posZ += 2;
+    if(moveForwards) posY += 2;
+    if(moveBackwards) posY -= 2;
+    if(rotateLeft) posR += 2;
+    if(rotateRight) posR -= 2;
+    positions = [];
+    var ang = posR * 0.0123;
+    var s = Math.sin(ang);
+    var c = Math.cos(ang);
+    // What does this do?
+    var matrix = [c, 0, -s, (posX * 0.123), 0, 1, 0, (posY * 0.123), s, 0, c, (posZ * 0.123)];
+    // console.log('X', posX, 'Y', posY, 'Z', posZ, 'R', posR);
+    ctx.clearRect(0, 0, height, width);
+    var [points, triangles] = getPositions(cubes);
+    var positions = draw(points, triangles, matrix);
+    positions.sort(sortZ);
+    paintFill(positions);
+    window.requestAnimationFrame(update);
 }
 
-function paintFill(pos) {
-  for (var i = 0; i < pos.length; i++) {
-    ctx.beginPath();
-    ctx.lineWidth = 5 / pos[i][0];
-    ctx.moveTo(pos[i][1], pos[i][4]);
-    ctx.lineTo(pos[i][2], pos[i][5]);
-    ctx.lineTo(pos[i][3], pos[i][6]);
-    ctx.lineTo(pos[i][1], pos[i][4]);
-    ctx.fillStyle = pos[i][7];
-    // ctx.stroke();
-    ctx.fill();
-  }
-}
-
-var _3 = 3;
-
-function draw(ps, ts, matrix, color) {
-  for (var i = 0; i < ts.length; i += 3) {
-    var p0 = ts[i] * _3;
-    var p1 = ts[i + 1] * _3;
-    var p2 = ts[i + 2] * _3;
-    var a = vertexShader(ps[p0], ps[p0 + 1], ps[p0 + 2], matrix);
-    var b = vertexShader(ps[p1], ps[p1 + 1], ps[p1 + 2], matrix);
-    var c = vertexShader(ps[p2], ps[p2 + 1], ps[p2 + 2], matrix);
-    fragmentShader(a, b, c, color[Math.floor(i / 36)]);
-  }
+function paintFill(positions) {
+    for (var i = 0; i < positions.length; i++) {
+        if (positions[i]) {
+            ctx.beginPath();
+            ctx.lineWidth = 5 / positions[i][0];
+            ctx.moveTo(positions[i][1], positions[i][4]);
+            ctx.lineTo(positions[i][2], positions[i][5]);
+            ctx.lineTo(positions[i][3], positions[i][6]);
+            ctx.lineTo(positions[i][1], positions[i][4]);
+            ctx.fillStyle = positions[i][7];
+            ctx.stroke();
+            ctx.fill();
+        }
+    }
 }
 
 function vertexShader(x, y, z, matrix) {
-  var x0 = matrix[0] * x + matrix[1] * y + matrix[2] * z + matrix[3];
-  var y0 = matrix[4] * x + matrix[5] * y + matrix[6] * z + matrix[7];
-  var z0 = matrix[8] * x + matrix[9] * y + matrix[10] * z + matrix[11];
-  return [x0, y0, z0];
+    var x0 = matrix[0] * x + matrix[1] * y + matrix[2] * z + matrix[3];
+    var y0 = matrix[4] * x + matrix[5] * y + matrix[6] * z + matrix[7];
+    var z0 = matrix[8] * x + matrix[9] * y + matrix[10] * z + matrix[11];
+    return [x0, y0, z0];
 }
 
 var _150 = 150;
 var _200 = 200;
 var _300 = 300;
-
 // you can make a Z-buffer, sampling from texture, phong shading...
-function fragmentShader(a, b, c, color) {
-  var z = Math.min(a[2], b[2], c[2]);
-  if (z < 0) return;
-  var x0 = _200 + _300 * a[0] / a[2];
-  var y0 = _150 + _300 * a[1] / a[2];
-  var x1 = _200 + _300 * b[0] / b[2];
-  var y1 = _150 + _300 * b[1] / b[2];
-  var x2 = _200 + _300 * c[0] / c[2];
-  var y2 = _150 + _300 * c[1] / c[2];
-  positions.push([z, x0, x1, x2, y0, y1, y2, color]);
+function fragmentShader(a, b, c) {
+    var z = Math.min(a[2], b[2], c[2]);
+    if (z < 0) return;
+    var x0 = _200 + _300 * a[0] / a[2];
+    var y0 = _150 + _300 * a[1] / a[2];
+    var x1 = _200 + _300 * b[0] / b[2];
+    var y1 = _150 + _300 * b[1] / b[2];
+    var x2 = _200 + _300 * c[0] / c[2];
+    var y2 = _150 + _300 * c[1] / c[2];
+    return [z, x0, x1, x2, y0, y1, y2, 'rgba(150, 150, 150, 1)'];
+}
+
+var _3 = 3;
+function draw(points, triangles, matrix) {
+    var positions = [];
+    for (var i = 0; i < triangles.length; i += 3) {
+        var p0 = triangles[i] * _3;
+        var p1 = triangles[i + 1] * _3;
+        var p2 = triangles[i + 2] * _3;
+        var a = vertexShader(points[p0], points[p0 + 1], points[p0 + 2], matrix);
+        var b = vertexShader(points[p1], points[p1 + 1], points[p1 + 2], matrix);
+        var c = vertexShader(points[p2], points[p2 + 1], points[p2 + 2], matrix);
+        positions.push(fragmentShader(a, b, c));
+    }
+    return positions;
 }
 
 function sortZ(a, b) {
-  return b[0] - a[0];
+    return b[0] - a[0];
 }
 
 update();
