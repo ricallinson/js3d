d04540ed13eb5b54c4d48286f94d69e2d345b7bd
Ric Allinson <rallinson@medicalert.org>
2022-12-13 20:28:31 -0800

    Got it working at last...

--- 3d.js
+++ 3d.js
@@ -5,10 +5,10 @@ var ctx = canvas.getContext("2d");
 var height = canvas.clientWidth;
 var width = canvas.clientHeight;
 
-var posX = 0; //15;
-var posY = 0; //5;
-var posZ = 0; //40;
-var posR = 0; //0;
+var posX = 15;
+var posY = 5;
+var posZ = 40;
+var posR = 0;
 
 var moveLeft = false;
 var moveRight = false;
@@ -58,47 +58,53 @@ function updateKeys(code,val) {
     }
 }
 
-function getPositions(models) {
+function getModelPositions(model) {
     var points = [];
     var triangles = [];
-    for (var i = 0; i < models.length; i++) {
-        points.push(...models[i].points);
-        triangles.push(...models[i].triangles);
+    for (var i = 0; i < model.length; i++) {
+        points.push(...model[i].points);
+        triangles.push(...model[i].triangles);
     }
     return [points, triangles];
 }
 
-function update(models) {
+function updateCameraLocation() {
     if(moveLeft) posX += 2;
     if(moveRight) posX -= 2;
-    if(moveUp) posY += 2;
-    if(moveDown) posY -= 2;
-    if(moveForwards) posZ -= 2;
-    if(moveBackwards) posZ += 2;
+    if(moveUp) posZ -= 2;
+    if(moveDown) posZ += 2;
+    if(moveForwards) posY += 2;
+    if(moveBackwards) posY -= 2;
     if(rotateLeft) posR -= 2;
     if(rotateRight) posR += 2;
+}
 
+function update(model) {
+    updateCameraLocation();
     var angle = posR * 0.0123;
-    var cos = Math.cos(angle);
-    var sin = Math.sin(angle);
-    var blockHeight = 1;
-    var matrix = [cos, 0, sin * -1, posX, 0, blockHeight, 0, posY, sin, 0, cos, posZ];
+    var cos = Math.cos(angle);// + posX;
+    var sin = Math.sin(angle);// + posY;
+    var matrix = [
+        cos,  null, -sin, posX,
+        null, null, null, posY,
+        sin,  null, cos,  posZ
+    ];
     console.log(matrix);
     ctx.clearRect(0, 0, height, width);
-    var [points, triangles] = getPositions(models);
+    var [points, triangles] = getModelPositions(model);
     draw(points, triangles, matrix);
     window.requestAnimationFrame(function() {
-        update(models);
+        update(model);
     });
 }
 
 function transform(x, y, z, matrix) {
-    // (cos * x) + (0 * y) + (-sin * z) + posX
-    var X = (matrix[0] * x) + /*(matrix[1] * y) +*/ (matrix[2] * z);// + matrix[3];
-    // (0 * x) + (blockHeight * y) + (0 * z) + posY
-    var Y = (matrix[4] * x) + (matrix[5] * y) + (matrix[6] * z) + matrix[7];
-    // (sin * x) + (0 * y) + (cos * z) + posZ
-    var Z = (matrix[8] * x) + /*(matrix[9] * y) +*/ (matrix[10] * z);// + matrix[11];
+    // (cos * x) + (-sin * z) + posX
+    var X = (matrix[0] * (x - matrix[3])) + (matrix[2] * (z - matrix[11]));
+    // y + posY
+    var Y = y + matrix[7];
+    // (sin * x) + (cos * z) + posZ
+    var Z = (matrix[8] * (x - matrix[3])) + (matrix[10] * (z - matrix[11]));
     // The transformed X,Y,Z.
     return [X, Y, Z];
 }
@@ -108,12 +114,12 @@ function fragmentShader(a, b, c) {
     var z = Math.min(a[2], b[2], c[2]);
     if (z < 0) return;
     var dist = 400;
-    var x0 = dist * a[0] / a[2];
-    var y0 = dist * a[1] / a[2];
-    var x1 = dist * b[0] / b[2];
-    var y1 = dist * b[1] / b[2];
-    var x2 = dist * c[0] / c[2];
-    var y2 = dist * c[1] / c[2];
+    var x0 = (dist * a[0] / a[2]);// + centerX;
+    var y0 = (dist * a[1] / a[2]);// + centerY;
+    var x1 = (dist * b[0] / b[2]);// + centerX;
+    var y1 = (dist * b[1] / b[2]);// + centerY;
+    var x2 = (dist * c[0] / c[2]);// + centerX;
+    var y2 = (dist * c[1] / c[2]);// + centerY;
     return [z, x0, x1, x2, y0, y1, y2, 'rgba(150, 150, 150, 1)'];
 }
 
